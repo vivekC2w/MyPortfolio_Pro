276ea8a15fb47418e421452545c54876e2c2709e
Vivek <vivek23699@gmail.com>
2023-06-12 14:04:57 +0530

    header options sorted

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -36,9 +36,9 @@ export default function Header() {
 
   const getHeaderOptionsClass = (index) => {
     let classes = "header-option";
-    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator";
+    if (index < TOTAL_SCREENS.length - 1) classes += " header-option-seperator";
 
-    if (selectedScreen === index) classes += "selected-header-option";
+    if (selectedScreen === index) classes += " selected-header-option";
     return classes;
   };
 
